194bbc2858c0e2345bfebe1e1b864d26c83f4c20
Bryce <brycejensenius@gmail.com>
2024-12-06 19:20:56 -0600

    Adding images back in correct location

---
+++ frontend/src/Footer.js
@@ -0,0 +1,17 @@
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="text-body-secondary py-3">
+                <div className="container">
+                    <p>Follow us at:</p>
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+                    <a href="https://x.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
